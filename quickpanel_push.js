e0014ec85b7492ee000a4ad63bb3d86e018bd583
ajsinfotech233 <71782370+ajsinfotech233@users.noreply.github.com>
2025-01-02 18:08:56 +0530

    Update quickpanel_push.js

--- quickpanel_push.js
+++ quickpanel_push.js
@@ -1,17 +1,19 @@
 self.addEventListener('push', event => {
     const data = event.data.json();
-    
+
     const title = data.title || "Default Title";
     const options = {
         body: data.message || "Default message",
         icon: data.icon || "/icon.png",
-        badge: data.badge || "/badge.png"
+        badge: data.badge || "/badge.png",
+        data: {
+            url: data.url || "https://www.rojgarresult.app"
+        }
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
 });
 
-// Handle notification click (open a URL or close the notification)
 self.addEventListener('notificationclick', event => {
     event.notification.close();
     event.waitUntil(
