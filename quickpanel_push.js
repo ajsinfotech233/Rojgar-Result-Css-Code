58dbe4153852b3d5ee70b0f935dad5e33a8df52d
ajsinfotech233 <71782370+ajsinfotech233@users.noreply.github.com>
2025-01-02 16:40:00 +0530

    Update quickpanel_push.js

--- quickpanel_push.js
+++ quickpanel_push.js
@@ -1,12 +1,9 @@
-let website_id = 131;
-let website_pixel_key = 'RaPxPWgbwHnqz5Wa';
-importScripts("https://www.quickpanel.in/pixel_service_worker.js");
 self.addEventListener('push', function(event) {
   const data = event.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: data.icon,
-    data: data.url,
+    data: data.url
   });
 });
 
